7b760d00b117ed0c494b1ad49b709614b8440121
AlecChng <AlecChng@gmail.com>
2015-04-30 15:21:13 -0700

    Map and Base

---
+++ BasedGods/Assets/Alec_Map_Base/Scripts/Buildplace.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Buildplace : MonoBehaviour {
+	//Tower should be built
+	public GameObject towerPrefab;
+
+	void OnMouseUpAsButton() {
+		GameObject g = (GameObject)Instantiate(towerPrefab);
+		g.transform.position = transform.position + Vector3.up;
+	}
+}
