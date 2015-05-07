9afabed65c9825895dc2b9b5d56f24f9313b3bd7
petegodkin <petegodkin@gmail.com>
2015-05-07 16:25:15 -0700

    Towers

---
+++ BasedGods/Assets/Towers/tutorial/Tower.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tower : MonoBehaviour {
+
+	public GameObject bulletPrefab;
+	public float rotationSpeed = 35;
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (Vector3.up * Time.deltaTime * rotationSpeed, Space.World);
+	}
+
+	void OnTriggerEnter(Collider co) {
+		if(co.GetComponent<Monster>()) {
+			GameObject g = (GameObject)Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+			g.GetComponent<Bullet>().target = co.transform;
+		}
+	}
+}
