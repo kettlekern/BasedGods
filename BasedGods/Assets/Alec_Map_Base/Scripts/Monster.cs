81d1c4fdc5f3c28719984245db4958f15f19df32
AlecChng <AlecChng@gmail.com>
2015-05-07 16:42:35 -0700

    Changed Castle

--- BasedGods/Assets/Alec_Map_Base/Scripts/Monster.cs
+++ BasedGods/Assets/Alec_Map_Base/Scripts/Monster.cs
@@ -6,14 +6,14 @@ public class Monster : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		// Navigate to Castle
-		GameObject castle = GameObject.Find("Castle");
+		GameObject castle = GameObject.Find("Base");
 		if (castle)
 			GetComponent<NavMeshAgent>().destination = castle.transform.position;
 	}
 
 	void OnTriggerEnter(Collider co) {
 		// If castle then deal Damage, destroy self
-		if (co.name == "Castle") {
+		if (co.name == "Base") {
 			co.GetComponentInChildren<Health>().decrease();
 			Destroy(gameObject);
 		}
