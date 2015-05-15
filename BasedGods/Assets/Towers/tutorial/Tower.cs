028a71dc540a60fbe1ba8a8196bdf9852a4cd74b
petegodkin <petegodkin@gmail.com>
2015-05-14 17:10:06 -0700

    Cooldown

--- BasedGods/Assets/Towers/tutorial/Tower.cs
+++ BasedGods/Assets/Towers/tutorial/Tower.cs
@@ -5,16 +5,22 @@ public class Tower : MonoBehaviour {
 
 	public GameObject bulletPrefab;
 	public float rotationSpeed = 35;
+	public float cooldown = 1.0f;
+	private float timer = 0.0f;
 
 	// Update is called once per frame
 	void Update () {
 		transform.Rotate (Vector3.up * Time.deltaTime * rotationSpeed, Space.World);
 	}
 
-	void OnTriggerEnter(Collider co) {
+	void OnTriggerStay(Collider co) {
 		if(co.GetComponent<Monster>()) {
-			GameObject g = (GameObject)Instantiate(bulletPrefab, transform.position, Quaternion.identity);
-			g.GetComponent<Bullet>().target = co.transform;
+			timer -= Time.deltaTime;
+			if(timer <= 0.0f) {
+				timer = cooldown;
+				GameObject g = (GameObject)Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+				g.GetComponent<Bullet>().target = co.transform;
+			}
 		}
 	}
 }
