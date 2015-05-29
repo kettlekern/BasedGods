fc4dd3ce3a99550ae45a4ff1648d773fc9bb43ea
petegodkin <petegodkin@gmail.com>
2015-05-29 15:23:52 -0700

    fixed tower

---
+++ BasedGods/Assets/Alec_Map_Base/Scripts/Monster2.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Monster2 : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		// Navigate to Castle
+		GameObject castle = GameObject.Find("Base 2");
+		if (castle)
+			GetComponent<NavMeshAgent>().destination = castle.transform.position;
+	}
+	
+	void OnTriggerEnter(Collider co) {
+		// If castle then deal Damage, destroy self
+		if (co.name == "Base 2") {
+			co.GetComponentInChildren<Health>().decrease();
+			Destroy(gameObject);
+		}
+	}
+	
+}
