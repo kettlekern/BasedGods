c8b8abce95c9d730a324e503f9444ebffc37f81d
petegodkin <petegodkin@gmail.com>
2015-05-29 13:44:53 -0700

    Fixed monster bug

--- BasedGods/Assets/Alec_Map_Base/Scripts/Monster.cs
+++ BasedGods/Assets/Alec_Map_Base/Scripts/Monster.cs
@@ -6,14 +6,14 @@ public class Monster : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		// Navigate to Castle
-		GameObject castle = GameObject.Find("Base");
+		GameObject castle = GameObject.Find("Base 1");
 		if (castle)
 			GetComponent<NavMeshAgent>().destination = castle.transform.position;
 	}
 
 	void OnTriggerEnter(Collider co) {
 		// If castle then deal Damage, destroy self
-		if (co.name == "Base") {
+		if (co.name == "Base 1") {
 			co.GetComponentInChildren<Health>().decrease();
 			Destroy(gameObject);
 		}
