8c8ad47c654946130fe58a4373ee514ecc7a740c
maxcrane <Max@pcp136073pcs.wireless.calpoly.edu>
2015-05-29 14:30:38 -0700

    Main Menu Goes to game
    added my own folder for scripts and a man menu with a working “new
    game” button only

---
+++ BasedGods/Assets/Scenes + max's scripts/MenuFunctionality.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuFunctionality : MonoBehaviour {
+	public string newGameScene;
+	// Use this for initialization
+	void Start () {
+		newGameScene = "AllBlocks";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ChangeToScene(){
+		Application.LoadLevel(newGameScene);
+	}
+}
